83cfe3605c415752cd3bbaa062e895e52d8274b0
JulietaAlfie <julietanurit@hotmail.com>
2022-04-27 22:15:07 -0300

    dockerfiles and Oauth2

---
+++ backendParcial/gateway/src/main/java/com/example/gateway/config/SecConfig.java
@@ -0,0 +1,25 @@
+package com.example.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
+//
+//@Configuration
+//public class SecConfig {
+//
+//    @Bean
+//    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository){
+//        http.oauth2Login();
+//
+//        http.logout(logout -> logout.logoutSuccessHandler(new OidcClientInitiatedServerLogoutSuccessHandler(
+//                clientRegistrationRepository)));
+//        http.authorizeExchange().anyExchange().authenticated();
+//        http.headers().frameOptions().mode(XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN);
+//        http.csrf().disable();
+//        return http.build();
+//    }
+//}
